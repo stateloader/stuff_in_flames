97e97888ca27ae621f218e91b84d741ea679db94
stateloader <78800629+stateloader@users.noreply.github.com>
2022-05-24 23:54:57 +0200

    Delete sensor/SensorReader directory

--- sensor/SensorReader/src/main/java/one/solution/sensorreader/backend/DataBase.java
+++
@@ -1,7 +0,0 @@
-package one.solution.sensorreader.backend;
-
-/*Klass redo för SQL-logik vid intresse. Lagt i samma paket (Backend) som DataFetcher för att underlätta "samarbete"*/
-
-public class DataBase {
-  // logik
-}
